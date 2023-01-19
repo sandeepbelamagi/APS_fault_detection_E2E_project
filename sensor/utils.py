eaa6635cd09b4c97fc8f55b27fa5b36b46dcc584
sandeepbelamagi <sandeepbelamagi28@gmail.com>
2023-01-19 11:25:35 +0530

    validation

--- sensor/utils.py
+++ sensor/utils.py
@@ -3,6 +3,7 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.config import mongo_client
 import os, sys
+import yaml
 
 def get_collection_as_dataframe(database_name:str,collection_name:str) -> pd.DataFrame:
     """
@@ -23,5 +24,14 @@ def get_collection_as_dataframe(database_name:str,collection_name:str) -> pd.Dat
             df = df.drop("_id",axis=1)
         logging.info(f"Row and columns in df: {df.shape}")
         return df
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
     except Exception as e:
         raise SensorException(e, sys)
